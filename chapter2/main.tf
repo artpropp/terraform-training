a87bc8c5b14ed4f826d13dcff039322cac912553
Artur Propp <artur.propp@me.com>
2020-09-17 19:01:24 +0200

    add security group with ingress

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -5,8 +5,22 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0c960b947cbb2dd16"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = "${file("simple-web-script.sh")}"
 
     tags = {
-        Name = "terraform-example"
+        Name = "terraform-example-rename"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
